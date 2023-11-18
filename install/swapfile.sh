d8112fd6a99e946efa5e4c4ffbc5f31bdd7e9241
Masayuki Takagi <kamonama@gmail.com>
2023-11-18 19:59:11 +0900

    First commit

---
+++ install/swapfile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+
+if [ -n "${skip_swap}" ]; then
+    exit 0
+fi
+
+if [ -e /swapfile ]; then
+    sudo swapoff /swapfile
+    sudo rm /swapfile
+fi
+
+set -e
+
+sudo dd if=/dev/zero of=/swapfile bs=1M count=8192
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+if ! grep -q /swapfile /etc/fstab; then
+    echo /swapfile swap swap defaults 0 0 | sudo tee -a /etc/fstab > /dev/null
+fi
